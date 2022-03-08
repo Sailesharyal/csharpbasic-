5781d102cc67ef805be6b1700524811ad5c720fb
sailesh aryal <sailesharyal500@gmail.com>
2022-03-08 17:43:55 +0545

    hello world

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template
+
+//first code
+
+Console.WriteLine("hello world");
+
+Console.WriteLine("my name is sailesh aryal");
+
+//declaring variables
+string name =  "sailesh";
+Console.WriteLine(name);
+
+//using more words
+Console.WriteLine("YOUR NAME IS " + name);
+// plus help us to print all 
+
+//String interpolation 
+Console.WriteLine($"my name is {name}");
+// by using $ sign and curly bracket in variable you can do string intrepolation
